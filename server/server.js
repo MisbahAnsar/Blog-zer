21c2f264e62f675b192faa4b750801cd917bb99d
MisbahAnsar <misbaansari444@gmail.com>
2025-02-04 00:25:04 +0530

    added

--- server/server.js
+++ server/server.js
@@ -16,19 +16,19 @@ const app = express();
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-    credentials: true, // Allow cookies and credentials
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
 // app.use(cors({
-//     origin: "https://blogzers69.vercel.app",
-//     credentials: true,
+//     origin: 'http://localhost:5173', // Replace with your frontend URL
+//     credentials: true, // Allow cookies and credentials
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //     allowedHeaders: ['Content-Type', 'Authorization'],
 // }));
+
+app.use(cors({
+    origin: "https://blogzers69.vercel.app",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 
